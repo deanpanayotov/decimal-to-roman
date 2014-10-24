2493bf6a9d28d846f5fe2464f62282ca98eb4fe9
Dean Panayotov <deanpanayotov@gmail.com>
2014-10-25 02:31:21 +0300

    Replaced the String variable with a StringBuilder in the convert method

--- src/dtr/Convertion.java
+++ src/dtr/Convertion.java
@@ -5,17 +5,18 @@ import java.util.Map.Entry;
 
 public class Convertion {
 	public static String convert(int number) {
-		String result = new String("");
-		Iterator<Entry<String, Integer>> itrerator = RomanNumbersMapping.getMap().entrySet().iterator();
+		StringBuilder stringBuilder = new StringBuilder();
+		Iterator<Entry<String, Integer>> itrerator = RomanNumbersMapping
+				.getMap().entrySet().iterator();
 		Entry<String, Integer> rN = itrerator.next();
 		while (true) {
 			if (number >= rN.getValue()) {
 				number -= rN.getValue();
-				result += rN.getKey();
+				stringBuilder.append(rN.getKey());
 			} else if (itrerator.hasNext()) {
 				rN = itrerator.next();
 			} else {
-				return result;
+				return stringBuilder.toString();
 			}
 		}
 	}
