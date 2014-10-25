87ede579cb66fd9a5708070a5393059543b214c1
Dean Panayotov <deanpanayotov@gmail.com>
2014-10-25 13:06:09 +0300

    Moved all the logic in a single file.

--- src/dtr/DecimalToRoman.java
+++ src/dtr/DecimalToRoman.java
@@ -1,13 +1,46 @@
 package dtr;
 
-public class DecimalToRoman {
-	public static void main(String[] args) {
-		Extraction e = new Extraction();
-//		e.decimalToRoman(33.11);
-//		e.decimalToRoman(123.456);
-//		e.decimalToRoman(444.12);
-		e.decimalToRoman(18.543);
-//		e.decimalToRoman(84);
+import java.util.Scanner;
+import java.util.Map.Entry;
 
+public class DecimalToRoman {
+	public static String convert(int number) {
+		StringBuilder stringBuilder = new StringBuilder();
+		for (Entry<String, Integer> romanDigit : RomanDigitsMapping.getMap()
+				.entrySet()) {
+			while (number >= romanDigit.getValue()) {
+				number -= romanDigit.getValue();
+				stringBuilder.append(romanDigit.getKey());
+			}
+		}
+		return stringBuilder.toString();
+	}
+	
+	public static double readDouble() {
+		Scanner scan = new Scanner(System.in);
+		System.out
+				.println("Insert a decimal number, which you want to convert to roman: ");
+		return scan.nextDouble();
+	}
+
+	public static int extractFraction(double decimalNumber) {
+		double dec = decimalNumber;
+		double dec2 = dec;
+		int i = 0;
+
+		for (;;) {
+			dec *= 10;
+			i++;
+			if (dec % 1 == 0)
+				return (int) (dec % (Math.max((int) (dec2), 1) * Math
+						.pow(10, i)));
+		}
+	}
+
+	public static void decimalToRoman(double decimal) {
+		DecimalToRoman c = new DecimalToRoman();
+		System.out.print("\nOriginal number:" + decimal + "\nNumber in roman:"
+				+ c.convert((int) decimal) + ","
+				+ (c.convert(extractFraction(decimal))).toLowerCase() + "\n");
 	}
 }
